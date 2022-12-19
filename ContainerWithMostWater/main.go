edfc811b3f06063c97d2c5018bbf2a9a50f20ec1
German <34959778+GoGerman@users.noreply.github.com>
2022-12-19 20:36:32 +0300

    test

--- ContainerWithMostWater/main.go
+++ ContainerWithMostWater/main.go
@@ -1,1 +1,31 @@
 package main
+
+import "fmt"
+
+func main() {
+	heigth := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	hei := maxArea(heigth)
+	fmt.Println(hei)
+}
+
+func maxArea(height []int) int {
+	var max, max1 int
+	for i := 0; i < len(height); i++ {
+		for j := i; j < len(height); j++ {
+			if height[j] < height[i] {
+				num := height[i] - (height[i] - height[j])
+				max = num * j
+				if max > max1 {
+					max1 = max
+				}
+			} else {
+				num := height[j] - (height[j] - height[i])
+				max = num * j
+				if max > max1 {
+					max1 = max
+				}
+			}
+		}
+	}
+	return max1
+}
